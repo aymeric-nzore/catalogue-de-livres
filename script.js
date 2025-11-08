4bdaaa3cd2a30869f60cfa315c6e2200de1e7b7f
aymeric-nzore <aymeric.nzore2024@esatic.edu.ci>
2025-11-08 08:04:08 +0000

    ajout du fichier json

--- script.js
+++ script.js
@@ -3,43 +3,73 @@ const searchInput = document.getElementById("search");
 const catalogue = document.getElementById("catalogue");
 
 // Charger le fichier XML
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "livres.xml", true);
-xhr.onload = function () {
-  if (xhr.status === 200) {
-    const xml = xhr.responseXML;
-    const livres = xml.getElementsByTagName("livre");
+// const xhr = new XMLHttpRequest();
+// xhr.open("GET", "livres.xml", true);
+// xhr.onload = function () {
+//   if (xhr.status === 200) {
+//     const xml = xhr.responseXML;
+//     const livres = xml.getElementsByTagName("livre");
 
-    // Boucle sur chaque livre
-    for (let i = 0; i < livres.length; i++) {
-      const livre = livres[i];
-      const titre = livre.getElementsByTagName("titre")[0].textContent;
-      const auteur = livre.getElementsByTagName("auteur")[0].textContent;
-      const annee = livre.getElementsByTagName("annee")[0].textContent;
-      const genre = livre.getElementsByTagName("genre")[0].textContent;
+//     // Boucle sur chaque livre
+//     for (let i = 0; i < livres.length; i++) {
+//       const livre = livres[i];
+//       const titre = livre.getElementsByTagName("titre")[0].textContent;
+//       const auteur = livre.getElementsByTagName("auteur")[0].textContent;
+//       const annee = livre.getElementsByTagName("annee")[0].textContent;
+//       const genre = livre.getElementsByTagName("genre")[0].textContent;
 
-      // Création du bloc HTML
-      const div = document.createElement("div");
-      div.className = "livre";
-      div.innerHTML = `
-        <h3>${titre}</h3>
-        <p><strong>Auteur :</strong> ${auteur}</p>
-        <p><strong>Année :</strong> ${annee}</p>
-        <p><strong>Genre :</strong> ${genre}</p>
-      `;
-      catalogue.appendChild(div);
-    }
-  }
-};
-xhr.send();
+//       // Création du bloc HTML
+//       const div = document.createElement("div");
+//       div.className = "livre";
+//       div.innerHTML = `
+//         <h3>${titre}</h3>
+//         <p><strong>Auteur :</strong> ${auteur}</p>
+//         <p><strong>Année :</strong> ${annee}</p>
+//         <p><strong>Genre :</strong> ${genre}</p>
+//       `;
+//       catalogue.appendChild(div);
+//     }
+//   }
+// };
+// xhr.send();
 
-// Recherche dynamique
-searchInput.addEventListener("input", () => {
-  const filtre = searchInput.value.toLowerCase();
+// // Recherche dynamique
+// searchInput.addEventListener("input", () => {
+//   const filtre = searchInput.value.toLowerCase();
+//   const livres = document.querySelectorAll(".livre");
+
+//   livres.forEach((livre) => {
+//     const texte = livre.textContent.toLowerCase();
+//     livre.style.display = texte.includes(filtre) ? "block" : "none";
+//   });
+// });
+
+
+// Avec un fichier json
+
+fetch("livres.json")
+  .then((reponse) => reponse.json())
+  .then((data) => {
+    const livres = data.livres;
+    livres.forEach((livre) => {
+      //Ma div qui contient mes livres
+      const livresDiv = document.createElement("div");
+      livresDiv.className = "livre";
+      livresDiv.innerHTML = `<h3>${livre.titre}</h3>
+        <p><strong>Auteur :</strong> ${livre.auteur}</p>
+        <p><strong>Année :</strong> ${livre.annee}</p>
+        <p><strong>Genre :</strong> ${livre.genre}</p>`;
+      catalogue.appendChild(livresDiv);
+    });
+  });
+
+searchInput.addEventListener("input" ,searchLivres)
+function searchLivres (){
+  const filter = searchInput.value.toLowerCase();
   const livres = document.querySelectorAll(".livre");
 
   livres.forEach(livre => {
-    const texte = livre.textContent.toLowerCase();
-    livre.style.display = texte.includes(filtre) ? "block" : "none";
+    const text = livre.textContent.toLowerCase();
+    livre.style.display = text.includes(filter) ? "block" : "none"
   });
-});
+}
